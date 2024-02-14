33a376207e517e1979fbdf0ed260fbe4c55c25b2
facundogarciaconejero <facundo.g@thoughtworks.com>
2024-02-14 20:41:47 +0100

    Adding open and close functionality to the main button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import FriendsList from "./components/FriendsList";
 import FormAddFriends from "./components/FormAddFriends";
 import Button from "./components/Button";
 import FormSplitBill from "./components/FormSplitBill";
+import { useState } from "react";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend(!showAddFriend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriends />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriends />}
+        <Button onClick={handleShowAddFriend}>
+          {!showAddFriend ? "Add friend" : "Close"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
