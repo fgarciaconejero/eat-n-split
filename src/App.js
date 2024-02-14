6b8797f34574febf7d07b37f4f0f5a0ab97678e1
facundogarciaconejero <facundo.g@thoughtworks.com>
2024-02-14 20:19:39 +0100

    Adding components to the main one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import FriendsList from "./components/FriendsList";
+import FormAddFriends from "./components/FormAddFriends";
+import Button from "./components/Button";
+import FormSplitBill from "./components/FormSplitBill";
 
 export default function App() {
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriends />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
