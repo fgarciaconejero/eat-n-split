11c7655c30cd91d4ba223991e20a10addbd7e893
facundogarciaconejero <facundo.g@thoughtworks.com>
2024-02-14 21:02:14 +0100

    Uplifted friends state

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,33 +1,11 @@
 import Friend from "./Friend";
 
-export default function FriendsList() {
-  const friends = initialFriends;
+export default function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.key} />
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
 }
-
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
